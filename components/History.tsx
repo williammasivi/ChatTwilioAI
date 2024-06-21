119b65b21151239b6db6d1f425b953c35d8d7da9
BirushaNdegeya <birushandegeya@gmail.com>
2024-06-21 14:08:38 +0200

    solve deployment issues

--- components/History.tsx
+++ components/History.tsx
@@ -1,17 +1,10 @@
-
-
-// components/History.js
-export default function History({ messages }: { messages: string[]| null }) {
+export default function History({ messages }: { messages: string[] | null }) {
    return (
-     <div className="p-4 bg-gray-100 rounded shadow">
-       <h2 className="text-lg font-bold mb-4">Chat History</h2>
-       <div className="space-y-4">
-         {/* {messages?.map(({sender, text}, index) => (
-           <div key={index} className={`p-2 rounded ${sender === "bot" ? "bg-indigo-100" : "bg-gray-200"}`}>
-             <p><strong>{sender}:</strong> {text}</p>
-           </div>
-         ))} */}
-       </div>
-     </div>
+      <div className="p-4 bg-gray-100 rounded shadow">
+         <h2 className="text-lg font-bold mb-4">Chat History</h2>
+         <div className="space-y-4">
+
+         </div>
+      </div>
    );
- }
+}
--- components/VoiceRecorder.tsx
+++ components/VoiceRecorder.tsx
@@ -1,62 +1,62 @@
-// components/VoiceRecorder.js
 import React, { useState, useEffect, useRef } from 'react';
 
+
 const VoiceRecorder = () => {
-  const [isRecording, setIsRecording] = useState(false);
-  const [audioURL, setAudioURL] = useState('');
-//   const mediaRecorder = useRef(null);
-  const mediaRecorder = useRef<MediaRecorder | null>(null);
-  const audioChunks = useRef([]);
+   const [isRecording, setIsRecording] = useState(false);
+   const [audioURL, setAudioURL] = useState('');
+   //   const mediaRecorder = useRef(null);
+   const mediaRecorder = useRef<MediaRecorder | null>(null);
+   const audioChunks = useRef([]);
 
-  useEffect(() => {
-    if (isRecording) {
-      startRecording();
-    } else {
-      stopRecording();
-    }
-  }, [isRecording]);
+   useEffect(() => {
+      if (isRecording) {
+         startRecording();
+      } else {
+         stopRecording();
+      }
+   }, [isRecording]);
 
-  const startRecording = () => {
-    navigator.mediaDevices.getUserMedia({ audio: true })
-      .then(stream => {
-        mediaRecorder.current = new MediaRecorder(stream);
-        mediaRecorder.current.start();
-        mediaRecorder.current.ondataavailable = (event) => {
-          audioChunks.current.push(event.data);
-        };
-        mediaRecorder.current.onstop = () => {
-          const audioBlob = new Blob(audioChunks.current, { type: 'audio/wav' });
-          const audioUrl = URL.createObjectURL(audioBlob);
-          setAudioURL(audioUrl);
-          audioChunks.current = [];
-        };
-      })
-      .catch(err => {
-        console.error("Error accessing microphone: ", err);
-      });
-  };
+   const startRecording = () => {
+      navigator.mediaDevices.getUserMedia({ audio: true })
+         .then(stream => {
+            mediaRecorder.current = new MediaRecorder(stream);
+            mediaRecorder.current.start();
+            // mediaRecorder.current.ondataavailable = (event: any) => {
+            //    audioChunks.current.push(event.data);
+            // };
+            // mediaRecorder.current.onstop = () => {
+            //    const audioBlob = new Blob(audioChunks.current, { type: 'audio/wav' });
+            //    const audioUrl = URL.createObjectURL(audioBlob);
+            //    setAudioURL(audioUrl);
+            //    audioChunks.current = [];
+            // };
+         })
+         .catch(err => {
+            console.error("Error accessing microphone: ", err);
+         });
+   };
 
-  const stopRecording = () => {
-    if (mediaRecorder.current) {
-      mediaRecorder.current.stop();
-    }
-  };
+   const stopRecording = () => {
+      if (mediaRecorder.current) {
+         mediaRecorder.current.stop();
+      }
+   };
 
-  return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <button
-        onClick={() => setIsRecording(!isRecording)}
-        className={`px-4 py-2 mb-4 text-white ${isRecording ? 'bg-red-500' : 'bg-blue-500'} rounded-lg`}
-      >
-        {isRecording ? 'Stop Recording' : 'Start Recording'}
-      </button>
-      {audioURL && (
-        <audio controls src={audioURL} className="mt-4">
-          Your browser does not support the audio element.
-        </audio>
-      )}
-    </div>
-  );
+   return (
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+         <button
+            onClick={() => setIsRecording(!isRecording)}
+            className={`px-4 py-2 mb-4 text-white ${isRecording ? 'bg-red-500' : 'bg-blue-500'} rounded-lg`}
+         >
+            {isRecording ? 'Stop Recording' : 'Start Recording'}
+         </button>
+         {audioURL && (
+            <audio controls src={audioURL} className="mt-4">
+               Your browser does not support the audio element.
+            </audio>
+         )}
+      </div>
+   );
 };
 
 export default VoiceRecorder;
