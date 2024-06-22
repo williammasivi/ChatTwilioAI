6fff73541d43f01095f6e4cefa346a0e97451dbe
BirushaNdegeya <birushandegeya@gmail.com>
2024-06-22 11:20:52 +0200

    preview gemini message from a backend server

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
    title: "chat-twilio-ai",
    description: "CHAT-TWILIO-API is an innovative voice-based application leveraging Twilio Voice and Gemini AI to deliver seamless, intelligent, and human-like voice interactions for customer service and automated assistance. ",
+   icons: {
+      icon: '/favicon.ico'
+   }
 };
 
 export default function RootLayout({ children }: Readonly<LayoutProps>) {
--- components/History.tsx
+++ components/History.tsx
@@ -8,3 +8,21 @@ export default function History({ messages }: { messages: string[] | null }) {
       </div>
    );
 }
+
+/*
+
+
+{messages.map((message, index) => (
+                     <div
+                        key={index}
+                        className={`flex ${message.sender === 'bot' ? 'justify-start' : 'justify-end'}`}
+                     >
+                        <div
+                           className={`${message.sender === 'bot' ? 'bg-gray-200' : 'bg-blue-500 text-white'} p-3 rounded-lg max-w-xs`}
+                        >
+                           {message.text}
+                        </div>
+                     </div>
+                  ))}
+               </div>
+*/
