e74c8422b3917850767bb3c4089cda10ccbee65d
BirushaNdegeya <birushandegeya@gmail.com>
2024-06-22 16:27:04 +0200

    Merge branch 'main' of https://github.com/BirushaNdegeya/ChatTwilioAi

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
 "use client";
-import History from '@/components/History';
 import { useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import Loading from './loading';
 import Goma from "@/components/Goma"
-import ReactMarkdown from 'react-markdown';
-
 
 const linkBACKEND = 'https://chattwilioai-backend.onrender.com/api/questions';
 
@@ -15,10 +12,12 @@ interface FormInputs {
 
 export default function Home() {
    const [error, setError] = useState("");
+   const [loading, setLoading] = useState(true);
    const [chatHistory, setChatHistory] = useState<Array<{ role: string; parts: string; }>>([]);
    const { register, handleSubmit, reset } = useForm<FormInputs>();
 
    const onSubmit: SubmitHandler<FormInputs> = async (data) => {
+      setLoading(true);
       if (!data.message.trim()) {
          setError('Error: Please enter something!');
          return;
@@ -46,25 +45,24 @@ export default function Home() {
                parts: result
             }];
          });
+         setLoading(false);
          reset();
       } catch (error) {
          setError('Something went wrong! Please try again later.');
+         setLoading(false);
       }
    };
    return (
       <div className='bg-gray-100 flex w-full h-full'>
-         <aside className='h-full border-b border-gray-200 bg-white'>
-            {/* <History messages={['hello', 'how are you?']} /> */}
-            <Goma />
+         <aside className='h-full border-b border-gray-200 bg-white hidden md:block'>
+            <MakeMessageCall />
          </aside>
          <aside className='flex-1 h-full w-full'>
             <div className="w-full h-full bg-white rounded-lg shadow-lg overflow-hidden flex-1">
-               <div className="p-4 border-b border-gray-200 bg-gray-50">
-                  <h1 className="text-lg font-semibold text-gray-800">CHAT TWILIO AI</h1>
-               </div>
                <div className="flex flex-col p-4 space-y-4 overflow-y-auto h-96">
                   {error && <p className='text-red-500 text-center'>{error}</p>}
                   <p>Hello! How can I help you today?</p>
+                  <Loading />
                   {chatHistory?.map((chatItem, index) => (
                      <div
                         key={index}
@@ -72,14 +70,11 @@ export default function Home() {
                      >
                         <p className={`p-4 rounded-md ${chatItem?.role == 'model' ? 'bg-gray-200' : 'bg-blue-500 text-white'}`}>
 
-                        <p>{chatItem?.role}:</p>
-                        <ReactMarkdown>{chatItem?.parts}</ReactMarkdown>
+                           {chatItem?.role} : {chatItem?.parts}
 
                         </p>
                      </div>
                   ))}
-                  <Loading />
-                  {/* <Goma /> */}
                </div>
                {/* form to send data to the server */}
                <div className="p-4 border-t border-gray-200 bg-gray-50">
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,26 @@
 "use client";
 import Link from "next/link";
+import Logo from "./Logo";
 import { useState } from "react";
 
 
 export default function Header() {
    const [isMenuOpen, setIsMenuOpen] = useState(false);
    return (
-      <header className="bg-indigo-600 p-4 shadow-md sticky top-0">
-         <div className="container mx-auto flex justify-between items-center">
-            <div className="flex items-center space-x-4">
-               <Link href="/" className="text-white text-xl font-bold">
-                  <h1>Twilio AI Chat</h1>
+      <header className="bg-blue-500 p-4 px-12 shadow-md sticky top-0">
+         <div className="flex items-center justify-between">
+            <Link href="/" className="text-white text-xl font-bold flex items-center">
+               <Logo />
+               <h1>Twilio AI Chat</h1>
+            </Link>
+            <nav className="hidden md:flex space-x-4">
+               <Link href="/" className="text-white hover:underline">
+                  <p>Home</p>
                </Link>
-               <nav className="hidden md:flex space-x-4">
-                  <Link href="/" className="text-white hover:underline">
-                     <p>Home</p>
-                  </Link>
-                  <Link href="/profile" className="text-white hover:underline">
-                     <p>Profile</p>
-                  </Link>
-                  <Link href="/chat" className="text-white hover:underline">
-                     <p>Chat</p>
-                  </Link>
-               </nav>
-            </div>
+               <Link href="/chat" className="text-white hover:underline">
+                  <p>Chat</p>
+               </Link>
+            </nav>
             <div className="md:hidden">
                <button
                   onClick={() => setIsMenuOpen(!isMenuOpen)}
@@ -47,12 +44,9 @@ export default function Header() {
             </div>
          </div>
          {isMenuOpen && (
-            <nav className="md:hidden bg-indigo-600">
+            <nav className="md:hidden bg-blue-500">
                <Link href="/" className="block text-white hover:underline px-4 py-2">
                   <p>Home</p>
-               </Link>
-               <Link href="/profile" className="block text-white hover:underline px-4 py-2">
-                  <p>Profile</p>
                </Link>
                <Link href="/chat" className="block text-white hover:underline px-4 py-2">
                   <p>Chat</p>
